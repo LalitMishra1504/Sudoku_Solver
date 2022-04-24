7e39311c8e6eb1062b94887a29936f741c09ec38
Lalit Mishra <lalitmishra7808@gmail.com>
2022-04-24 19:24:52 +0530

    Action Part

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import './styles/App.scss';
 import Banner from './component/Banner';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Action from './component/Action';
-import Block from './component/Block';
-import Footer from './component/Footer';
 function App() {
     return (
         <div className='App'>
